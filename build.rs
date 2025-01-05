42ea00ca99e271c22590e83d807a6a3f9c8c20ad
ElenJay <ihorlutsiuk@gmail.com>
2025-01-05 11:11:39 +0200

    Add "build.rs", to inclute "assets" folder in the built target

---
+++ build.rs
@@ -0,0 +1,38 @@
+use std::{
+    env, 
+    fs, 
+    path::{
+        Path, 
+        PathBuf, 
+    }, 
+};
+
+const COPY_DIR: &str = "assets";
+
+fn copy_dir<P, Q>(from: P, to: Q) where P: AsRef<Path>, Q: AsRef<Path> {
+    let to: PathBuf = to.as_ref().to_path_buf();
+
+    for path in fs::read_dir(from).unwrap() {
+        let path: PathBuf = path.unwrap().path();
+        let to: PathBuf = to.clone().join(path.file_name().unwrap());
+
+        if path.is_file() {
+            fs::copy(&path, to).unwrap();
+        } else if path.is_dir() {
+            if !to.exists() {
+                fs::create_dir(&to).unwrap();
+            }
+            copy_dir(&path, to);
+        }
+    }
+}
+
+fn main() {
+    let out  = PathBuf::from(format!("target/{}/{}", env::var("PROFILE").unwrap(), COPY_DIR));
+
+    if out.exists() {
+        fs::remove_dir_all(&out).unwrap();
+    }
+    fs::create_dir(&out).unwrap();
+    copy_dir(COPY_DIR, &out);
+}
